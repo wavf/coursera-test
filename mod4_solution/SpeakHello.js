db8a08f0d310e72aa60d11b77f3b7178d0e97f86
wavf <47406054+wavf@users.noreply.github.com>
2021-02-05 19:52:01 -0300

    Update SpeakHello.js

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function () {
   // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
   // See Lecture 52, part 2
 
@@ -24,4 +24,4 @@
   
   window.helloSpeaker = helloSpeaker;
 
-})(window);
+})();
